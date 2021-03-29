43cb32d4251107ddd211897816a8e67b06966ea5
smob123 <smob123@hotmail.com>
2021-03-29 21:20:42 +0300

    updated project dependencies, and fixed compiling errors

--- lib/widgets/movie_details_widgets/videoWidget.dart
+++ lib/widgets/movie_details_widgets/videoWidget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_youtube/flutter_youtube.dart';
+import 'package:ext_video_player/ext_video_player.dart';
 
-import '../../config/config.dart';
 import '../../networking/dataFetch.dart';
 
 class VideoWidget extends StatefulWidget {
@@ -13,6 +12,7 @@ class VideoWidget extends StatefulWidget {
 }
 
 class VideoWidgetState extends State<VideoWidget> {
+  VideoPlayerController _controller;
   String _youtubeVideoId; //the actual video's ID on YouTube
   Image _thumbnail; //the video's thumbnail
 
@@ -28,6 +28,7 @@ class VideoWidgetState extends State<VideoWidget> {
     //make sure it's not null, then get the video's thumbnail
     if (_youtubeVideoId != null) {
       _getThumbnail();
+      _initializeVideo();
     }
   }
 
@@ -41,39 +42,76 @@ class VideoWidgetState extends State<VideoWidget> {
     });
   }
 
-  _playVideo() {
+  _initializeVideo() {
     //play the video on full screen mode
-    FlutterYoutube.playYoutubeVideoById(
-        apiKey: apiData['youtube api key'],
-        videoId: _youtubeVideoId,
-        autoPlay: true,
-        fullScreen: true);
+    _controller = VideoPlayerController.network(
+        'https://www.youtube.com/watch?v=$_youtubeVideoId')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  List<Widget> _videoWidget() {
+    if (!_controller.value.isPlaying) {
+      return [
+        GestureDetector(
+          //make the thumbnail clickable
+          onTap: () {
+            setState(() {
+              _controller.play();
+            });
+          },
+          child: Container(
+            width: MediaQuery.of(context).size.width / 1.1,
+            margin: EdgeInsets.only(right: 5.0, left: 10.0),
+            height: 200.0,
+            color: Colors.black,
+            child: _thumbnail,
+          ),
+        ),
+        Positioned(
+          //a play button on top of the thumbnail
+          left: MediaQuery.of(context).size.width / 2.4,
+          child: FloatingActionButton(
+            onPressed: null,
+            backgroundColor: Colors.black26,
+            child: Icon(Icons.play_arrow),
+          ),
+        )
+      ];
+    }
+
+    return [
+      GestureDetector(
+        //make the thumbnail clickable
+        onTap: () {
+          setState(() {
+            _controller.pause();
+          });
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width / 1.1,
+          margin: EdgeInsets.only(right: 5.0, left: 10.0),
+          height: 200.0,
+          color: Colors.black,
+          child: VideoPlayer(_controller),
+        ),
+      )
+    ];
   }
 
   @override
   build(BuildContext context) {
-    return _youtubeVideoId == null //check if the video was fetched
-        ? Container()
-        : Stack( //contains the thumbnail, and a play icon on top of it
-        alignment: Alignment.center, children: [
-            GestureDetector( //make the thumbnail clickable
-              onTap: () => _playVideo(), //play the video on tap
-              child: Container(
-                width: MediaQuery.of(context).size.width / 1.1,
-                margin: EdgeInsets.only(right: 5.0, left: 10.0),
-                height: 200.0,
-                color: Colors.black,
-                child: _thumbnail,
-              ),
-            ),
-            Positioned( //a play button on top of the thumbnail
-              left: MediaQuery.of(context).size.width / 2.4,
-              child: FloatingActionButton(
-                onPressed: null,
-                backgroundColor: Colors.black26,
-                child: Icon(Icons.play_arrow),
-              ),
-            )
-          ]);
+
+    //check if the video was fetched
+    if(_youtubeVideoId == null || !_controller.value.initialized) {
+      return Container();
+    }
+
+    return Stack(
+            //contains the thumbnail, and a play icon on top of it
+            alignment: Alignment.center,
+            children: _videoWidget());
   }
 }
